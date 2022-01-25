8a3ad01e80f2dee0f33afd75cedfe5f8993d2a23
Animorama <jmora.aparicio@gmail.com>
2022-01-25 20:36:17 +0100

    Add Unity Project to Repo

---
+++ Assets/_Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator _animator;
+    private PlayerInput _input;
+    private PlayerMovement _movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool("Walk", _movement.IsMoving);
+        _animator.SetBool("Sneak", _input.Sneak);
+    }
+
+}
