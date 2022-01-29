9f216d73efb7db75e019652a3b6d00f0b240e1be
alejandro-rf <arenones@edu.tecnocampus.cat>
2022-01-29 11:58:04 +0100

    Add EnemySeePatrol

---
+++ Assets/_Scripts/Enemies/EnemySeePatrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySeePatrol : MonoBehaviour
+{
+    [SerializeField] float Speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if WallDetected() then Flip()
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(1,0,0) * Time.deltaTime * Speed;
+    }
+
+    private void Flip()
+    {
+        int angle = Random.Range(90, 271); // 271 is exclusive
+        transform.Rotate(new Vector3(0, angle, 0));
+    }
+}
