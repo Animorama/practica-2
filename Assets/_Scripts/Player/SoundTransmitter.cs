80e3b8a73609062e3ef7693c3e9e499240045916
Animorama <jmora.aparicio@gmail.com>
2022-02-06 12:40:22 +0100

    n

--- Assets/_Scripts/Player/SoundTransmitter.cs
+++ Assets/_Scripts/Player/SoundTransmitter.cs
@@ -8,9 +8,6 @@ public class SoundTransmitter : MonoBehaviour, IMakeNoise
     public float Noise => _noise;
     private float _noise;
 
-    //Function parameters
-    private Rigidbody2D _rigidBody;
-
     public void EmitNoise(Rigidbody2D _rigidBody)
     {
         _noise = _rigidBody.velocity.magnitude;
